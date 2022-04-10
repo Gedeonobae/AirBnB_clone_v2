203b87e7b7f14f5e0c7340e41c028fa815c8695d
Gedeonobae <gideonobae@gmail.com>
2022-04-10 23:27:34 +0300

    add web_flask directory

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,7 +3,8 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
 """
 from flask import Flask
 
@@ -12,8 +13,14 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
 if __name__ == "__main__":
